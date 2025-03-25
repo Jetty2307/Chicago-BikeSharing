e0695cdfcfe46a23971aba8c65b7b02ff13f8754
Victor Tkachenko <victor.tkachenko@desy.de>
2025-03-25 16:52:09 +0100

    Class extended

--- backend.py
+++ backend.py
@@ -13,6 +13,8 @@ from sklearn.model_selection import train_test_split, GridSearchCV
 #from xgboost import XGBClassifier, XGBRegressor
 from xgboost import XGBRegressor
 from sklearn.base import BaseEstimator
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
 
 
 # Configure logging
@@ -81,11 +83,17 @@ def root():
     return {"message": "Chicago Bike Sharing forecast API"}
 
 class interval_prop:
-    def __init__(self, dataframe, period):
+    def __init__(self, dataframe, period, offset, date_format):
         self.dataframe = dataframe
         self.period = period
+        self.offset = offset
+        self.date_format = date_format
 
-    def make_predictions(self, steps, interval, grid, rideable_type):
+    def add_interval(self, date_str):
+        date_obj = datetime.strptime(date_str, self.date_format)
+        new_date = date_obj + self.offset
+        return new_date.strftime(self.date_format)
+    def make_prediction(self, steps, interval, grid, rideable_type):
 
         df = self.dataframe[self.dataframe.rideable_type == rideable_type].reset_index(drop=True)
 
@@ -101,7 +109,7 @@ class interval_prop:
             current_season = last_row['season']
 
         if current_interval > self.period:  # Increment the year if the month exceeds 12
-            current_month = 1
+            current_interval = 1
             current_year = last_row['year'] + 1
             current_season = 0
         else:
@@ -110,15 +118,17 @@ class interval_prop:
         current_ride_id_count = last_2row['rides']  # Use as starting value for ride_id_count_lastmonth
         current_ride_id_count_plusinterval = last_row['rides']
 
+        current_year_interval = self.add_interval(last_row[f'year_{interval}'])
+
         for _ in range(steps):
             # Prepare the input features for prediction
             input_features = pd.DataFrame({
                 'rideable_type': [df['rideable_type'][0]],
                 'year': [current_year],
-                'interval': [current_interval],
+                interval: [current_interval],
                 'season': [current_season],
-                'rides_2months_ago': [current_ride_id_count],
-                'rides_lastinterval': [current_ride_id_count_plusinterval]
+                f'rides_2{interval}s_ago': [current_ride_id_count],
+                f'rides_last{interval}': [current_ride_id_count_plusinterval]
             })
 
             # Predict the ride_id_count for the current month
@@ -127,11 +137,12 @@ class interval_prop:
             # Append the forecasted data
             forecast_data.append({
                 'rideable_type': df['rideable_type'][0],
+                'time' : current_year_interval,
                 'year': current_year,
-                'interval': current_interval,
+                interval : current_interval,
                 'season': current_season,
-                'rides_2months_ago': current_ride_id_count,
-                'rides_lastinterval': current_ride_id_count_plusinterval,
+                f'rides_2{interval}s_ago': current_ride_id_count,
+                f'rides_last{interval}': current_ride_id_count_plusinterval,
                 'rides': predicted_ride_id_count
             })
 
@@ -150,6 +161,8 @@ class interval_prop:
                 current_year += 1
                 current_season = 0
 
+            current_year_interval = self.add_interval(current_year_interval)
+
         # Create a DataFrame from the forecasted data
         forecast_df = pd.DataFrame(forecast_data)
 
@@ -158,38 +171,41 @@ class interval_prop:
         return forecast_df
 
 
-month = interval_prop(dataframe=df_month, period=12)
-week = interval_prop(dataframe=df_week, period=52)
+month = interval_prop(
+    dataframe=df_month,
+    period=12,
+    offset=relativedelta(months=1),
+    date_format="%Y-%m"
+)
 
-
-def make_time_column(forecast, interval):
-    forecast['time'] = forecast['year'].astype(int).astype(str) + '-' + \
-    forecast[f'{str(interval)}'].astype(int).astype(str).str.zfill(2)
+week = interval_prop(
+    dataframe=df_week,
+    period=52,
+    offset=relativedelta(weeks=1),
+    date_format="%Y-%m-%d"
+)
 
 def merge_columns(forecast_classic, forecast_electric, interval):
-    make_time_column(forecast_classic, interval)
-    make_time_column(forecast_electric, interval)
 
     forecast_full = pd.DataFrame()
 
     # Iterate through columns
     for col in forecast_classic.columns:
-        if col in ['rides', 'rides_lastmonth', 'rides_2months_ago', 'rides_lastweek',
-                   'rides_2weeks_ago']:
+        if col in ['rides', f'rides_last{interval}', f'rides_2{interval}s_ago']:
             # Sum numeric columns
-
             forecast_full[col] = forecast_classic[col] + forecast_electric[col]
         else:
             forecast_full[col] = forecast_classic[col]
 
     return forecast_full
 
-def fit_xgboost(df):
+def fit_xgboost(df, interval):
     logger.debug(f"Input DataFrame shape: {df.shape}")
     logger.debug(f"Columns: {df.columns}")
     y = df['rides']
     #X = pd.get_dummies(df.drop(columns=['year_month', 'ride_id_count']), drop_first=True)
-    X = df.drop(columns=['year_month', 'rides'], axis=1)
+    # X = df.drop(columns=[f'year_{interval}', 'rides'], axis=1)
+    X = df[['rideable_type', 'year', f'{interval}', 'season', f'rides_2{interval}s_ago', f'rides_last{interval}']]
     logger.debug(f"Target (y) shape: {y.shape}, Features (X) shape: {X.shape}")
     logger.debug(f"Feature preview:\n{X.head()}")
     X_train, X_valid, y_train, y_valid = train_test_split(X, y, train_size=0.8, test_size=0.2, shuffle=True,
@@ -220,7 +236,8 @@ def fit_GAM(df, interval):
     logger.debug(f"Input DataFrame shape: {df.shape}")
     logger.debug(f"Columns: {df.columns}")
     y = df['rides']
-    X = df.drop(columns=[f"year_{str(interval)}", 'rides'], axis=1)
+    # X = df.drop(columns=[f'year_{interval}', 'rides'], axis=1)
+    X = df[['rideable_type', 'year', f'{interval}', 'season', f'rides_2{interval}s_ago', f'rides_last{interval}']]
     logger.debug(f"Target (y) shape: {y.shape}, Features (X) shape: {X.shape}")
     logger.debug(f"Feature preview:\n{X.head()}")
     model = GAM().fit(X, y)
@@ -234,6 +251,35 @@ def to_final_pd(d1, path_nonindexed):
         "forecast": forecast.to_dict(orient='records')  # Convert forecast to list for JSON serialization
     }
 
+def forecast_rides_regressive(request: ForecastRequest, model):
+    if request.interval == "week":
+        timeframe = week
+    elif request.interval == "month":
+        timeframe = month
+
+    df_full = timeframe.dataframe
+
+    path = timeframe.dataframe.groupby(f'year_{request.interval}')["rides"].sum()
+    path_nonindexed = path.reset_index()
+
+    try:
+        fulldata = model
+    except Exception as e:
+        logger.error(f"Error fitting model: {e}")
+        raise HTTPException(status_code=500, detail=f"Error fitting model: {str(e)}")
+
+    forecast_1_tot = timeframe.make_prediction(steps=request.steps,
+                                                    interval=request.interval, grid=fulldata, rideable_type=1)
+    forecast_2_tot = timeframe.make_prediction(steps=request.steps,
+                                                    interval=request.interval, grid=fulldata, rideable_type=2)
+    forecast_tot = merge_columns(forecast_1_tot, forecast_2_tot, interval=request.interval)
+    forecast_tot = forecast_tot[['time','rides']]
+
+    d1 = {f'year_{request.interval}': forecast_tot['time'], 'rides': forecast_tot['rides'].astype(int)}
+
+    return(to_final_pd(d1, path_nonindexed))
+
+
 @app.post("/forecast_bikes_sarima")
 def forecast_rides_sarima(request: ForecastRequest):
     # Prepare and fit SARIMAX model
@@ -245,7 +291,7 @@ def forecast_rides_sarima(request: ForecastRequest):
 
     path = timeframe.dataframe.groupby(f'year_{request.interval}')["rides"].sum()
     path_nonindexed = path.reset_index()
-    # path_nonindexed = df_week[['year_week','rides']]
+
     model = SARIMAX(path, order=(1, 1, 1), seasonal_order=(1, 1, 1, timeframe.period))
 
     results = model.fit(disp=False)
@@ -270,8 +316,20 @@ def forecast_rides_xgboost(request: ForecastRequest):
     elif request.interval == "month":
         timeframe = month
 
+    model = fit_xgboost(timeframe.dataframe, request.interval)
+    return forecast_rides_regressive(request, model)
+
+
+'''def forecast_rides_xgboost(request: ForecastRequest):
+    if request.interval == "week":
+        timeframe = week
+    elif request.interval == "month":
+        timeframe = month
+
     df_full = timeframe.dataframe
-    path_nonindexed = df_full[[f'year_{request.interval}', 'rides']]
+
+    path = timeframe.dataframe.groupby(f'year_{request.interval}')["rides"].sum()
+    path_nonindexed = path.reset_index()
 
     try:
         fulldata_xgboost = fit_xgboost(df_full)
@@ -279,39 +337,50 @@ def forecast_rides_xgboost(request: ForecastRequest):
         logger.error(f"Error fitting XGBoost model: {e}")
         raise HTTPException(status_code=500, detail=f"Error fitting XGBoost model: {str(e)}")
 
-    forecast_1_xgboost = timeframe.make_predictions(steps=request.steps,
+    forecast_1_xgboost = timeframe.make_prediction(steps=request.steps,
                                                     interval=request.interval, grid=fulldata_xgboost, rideable_type=1)
-    forecast_2_xgboost = timeframe.make_predictions(steps=request.steps,
+    forecast_2_xgboost = timeframe.make_prediction(steps=request.steps,
                                                     interval=request.interval, grid=fulldata_xgboost, rideable_type=2)
     forecast_xgboost = merge_columns(forecast_1_xgboost, forecast_2_xgboost)
     forecast_xgboost = forecast_xgboost[['time','rides']]
 
     d1 = {f'year_{request.interval}': forecast_xgboost['time'], 'rides': forecast_xgboost['rides'].astype(int)}
 
-    return(to_final_pd(d1, path_nonindexed))
+    return(to_final_pd(d1, path_nonindexed)) '''
 
 @app.post("/forecast_bikes_gam")
-def forecast_rides_gam(request: ForecastRequest):
+def forecast_rides_GAM(request: ForecastRequest):
+    if request.interval == "week":
+        timeframe = week
+    elif request.interval == "month":
+        timeframe = month
+
+    model = fit_GAM(timeframe.dataframe, request.interval)
+    return forecast_rides_regressive(request, model)
+
+'''def forecast_rides_gam(request: ForecastRequest):
     if request.interval == "week":
         timeframe = week
     elif request.interval == "month":
         timeframe = month
 
     df_full = timeframe.dataframe
-    path_nonindexed = df_full[[f'year_{request.interval}', 'rides']]
+
+    path = timeframe.dataframe.groupby(f'year_{request.interval}')["rides"].sum()
+    path_nonindexed = path.reset_index()
 
     fulldata_GAM = fit_GAM(df_full, interval=request.interval)
     forecast_1_GAM = timeframe.make_prediction(steps=request.steps,
                                                interval=request.interval, grid=fulldata_GAM, rideable_type=1)
-    forecast_2_GAM = timeframe.make_predictions(steps=request.steps,
-                                                interval=request.interval, grid=fulldata_GAM, rideable_type=2)
+    forecast_2_GAM = timeframe.make_prediction(steps=request.steps,
+                                               interval=request.interval, grid=fulldata_GAM, rideable_type=2)
 
     forecast_GAM = merge_columns(forecast_1_GAM, forecast_2_GAM, interval=request.interval)
     forecast_GAM = forecast_GAM[['time','rides']]
 
     d1 = {f'year_{request.interval}': forecast_GAM['time'], 'rides': forecast_GAM['rides'].astype(int)}
 
-    return(to_final_pd(d1, path_nonindexed))
+    return(to_final_pd(d1, path_nonindexed)) '''
 
 
 if __name__ == "__main__":

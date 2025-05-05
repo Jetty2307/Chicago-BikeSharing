ce91e43449d10b105b41f34debc20d93c430d89d
Jetty2307 <vbtka@mail.ru>
2025-05-06 01:01:44 +0200

    divvy_cleaning

--- divvy_download.py
+++ divvy_download.py
@@ -1,8 +1,3 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
 
 from urllib.request import urlopen
 from zipfile import ZipFile
@@ -43,7 +38,6 @@ for zip_url in zip_urls:
         with open(zip_filename, 'wb') as zip_file:
             zip_file.write(zip_response.content)
 
-        # Step 5: Extract the ZIP file
         with ZipFile(zip_filename, 'r') as zip_ref:
             print(f"Extracting: {zip_filename}")
             zip_ref.extractall(extracted_dir)

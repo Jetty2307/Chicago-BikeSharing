c4042922e56e8efa3ffef9712d8f731c0b1d314c
Victor Tkachenko <victor.tkachenko@desy.de>
2026-02-25 21:57:12 +0100

    Delete union_all.sql

--- union_all.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO merged (
-ride_id, rideable_type, started_at
-)
-SELECT ride_id::TEXT, rideable_type::TEXT, started_at::TIMESTAMP FROM "202512-divvy"
-UNION ALL
-SELECT ride_id::TEXT, rideable_type::TEXT, started_at::TIMESTAMP FROM "202601-divvy";
